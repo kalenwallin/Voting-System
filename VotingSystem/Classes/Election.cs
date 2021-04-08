861bd1dc6725ba5a1129900e3b6cca85045fe5cf
Josh Bellmyer <joshbellmyer@gmail.com>
2021-04-08 14:17:07 -0500

    Started on the Election class.
    - Getters and setters for both candidates and election type.
    - Constructor with parameters.
    - ToString method.

---
+++ VotingSystem/Classes/Election.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/*
+ * A class representing an election
+ * 
+ * Josh Bellmyer    4/8/2021
+ */
+public class Election {
+
+    public enum ElectionType {
+        Mayor,
+        CouncilMember,
+        Treasurer,
+    }
+
+    public string Candidate1 { get; private set; }
+    public string Candidate2 { get; private set; }
+    public ElectionType Type { get; private set; }
+
+
+    public Election (string candidate1, string candidate2, ElectionType type) {
+        Candidate1 = candidate1;
+        Candidate2 = candidate2;
+        Type = type;
+    }
+
+    public override string ToString () {
+        return $"{Candidate1} vs {Candidate2}";
+    }
+}
