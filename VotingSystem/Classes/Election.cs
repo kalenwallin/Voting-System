72c9c01826983c6cc27c7f374fba084486f2642f
Josh Bellmyer <joshbellmyer@gmail.com>
2021-04-09 16:40:05 -0500

    Modified Elelection class, Added Issue class

--- VotingSystem/Classes/Election.cs
+++ VotingSystem/Classes/Election.cs
@@ -1,30 +1,27 @@
-﻿using System;
+﻿namespace VotingSystem.Classes {
 
-/*
- * A class representing an election
- * 
- * Josh Bellmyer    4/8/2021
- */
-public class Election {
+    /*
+     * A class representing an election
+     * 
+     * Josh Bellmyer    4/9/2021
+     */
+    public class Election {
 
-    public enum ElectionType {
-        Mayor,
-        CouncilMember,
-        Treasurer,
-    }
-
-    public string Candidate1 { get; private set; }
-    public string Candidate2 { get; private set; }
-    public ElectionType Type { get; private set; }
+        public string Candidate1 { get; private set; }
+        public string Candidate2 { get; private set; }
+        public string ElectionType { get; private set; }
+        public int Year { get; private set; }
 
 
-    public Election (string candidate1, string candidate2, ElectionType type) {
-        Candidate1 = candidate1;
-        Candidate2 = candidate2;
-        Type = type;
-    }
+        public Election (string candidate1, string candidate2, string electionType, int year) {
+            Candidate1 = candidate1;
+            Candidate2 = candidate2;
+            ElectionType = electionType;
+            Year = year;
+        }
 
-    public override string ToString () {
-        return $"{Candidate1} vs {Candidate2}";
+        public override string ToString () {
+            return $"{ElectionType} {Year} ({Candidate1} vs {Candidate2})";
+        }
     }
 }
---
+++ VotingSystem/Classes/Issue.cs
@@ -0,0 +1,25 @@
+﻿namespace VotingSystem.Classes {
+
+    /*
+     * A class representing an issue to be voted on
+     * 
+     * Josh Bellmyer    4/9/2021
+     */
+    public class Issue {
+
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public int Year { get; private set; }
+
+
+        public Issue (string title, string description, int year) {
+            Title = title;
+            Description = description;
+            Year = year;
+        }
+
+        public override string ToString () {
+            return $"{Title} {Year}";
+        }
+    }
+}
