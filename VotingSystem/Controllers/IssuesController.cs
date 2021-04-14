543381c1c851d37b240bbd6b53dad6173236367f
JCarlon02 <jayden.carlon13@gmail.com>
2021-04-14 12:02:23 -0500

    Added CRUD
    Adding CRUD for Elections controller, Issues Controller, and Users controller

---
+++ VotingSystem/Controllers/IssuesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VotingSystem.Data;
+using VotingSystem.Models;
+
+namespace VotingSystem.Controllers
+{
+    public class IssuesController : Controller
+    {
+        private readonly VotingSystemContext _context;
+
+        public IssuesController(VotingSystemContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Issues
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Issues.ToListAsync());
+        }
+
+        // GET: Issues/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var issue = await _context.Issues
+                .FirstOrDefaultAsync(m => m.ElectionID == id);
+            if (issue == null)
+            {
+                return NotFound();
+            }
+
+            return View(issue);
+        }
+
+        // GET: Issues/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Issues/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ElectionID,VotesFor,VotesAgainst,Title,Description")] Issue issue)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(issue);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(issue);
+        }
+
+        // GET: Issues/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var issue = await _context.Issues.FindAsync(id);
+            if (issue == null)
+            {
+                return NotFound();
+            }
+            return View(issue);
+        }
+
+        // POST: Issues/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ElectionID,VotesFor,VotesAgainst,Title,Description")] Issue issue)
+        {
+            if (id != issue.ElectionID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(issue);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!IssueExists(issue.ElectionID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(issue);
+        }
+
+        // GET: Issues/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var issue = await _context.Issues
+                .FirstOrDefaultAsync(m => m.ElectionID == id);
+            if (issue == null)
+            {
+                return NotFound();
+            }
+
+            return View(issue);
+        }
+
+        // POST: Issues/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var issue = await _context.Issues.FindAsync(id);
+            _context.Issues.Remove(issue);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool IssueExists(int id)
+        {
+            return _context.Issues.Any(e => e.ElectionID == id);
+        }
+    }
+}
