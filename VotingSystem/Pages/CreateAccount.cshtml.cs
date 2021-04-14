02d2221963e5c6434e44e3af6709aa7bae991424
Kalen Wallin <kwallin10@huskers.unl.edu>
2021-04-14 17:59:54 -0500

    Added login and create account failed messages

--- VotingSystem/Pages/CreateAccount.cshtml.cs
+++ VotingSystem/Pages/CreateAccount.cshtml.cs
@@ -6,30 +6,41 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using VotingSystem.Classes;
+using VotingSystem.Controllers;
 
 namespace VotingSystem.Pages.Shared
 {
     public class CreateAccountModel : PageModel
     {
-        [BindProperty] 
-        [StringLength(60, MinimumLength = 5, ErrorMessage = "Full Name must be between 5 and 60 characters.")] 
-        [Required]  public string Name { get; set; }
+        [BindProperty]
+        [StringLength(60, MinimumLength = 5, ErrorMessage = "Full Name must be between 5 and 60 characters.")]
+        [Required] public string Name { get; set; }
 
-        [BindProperty] 
-        [StringLength(60, MinimumLength = 8, ErrorMessage = "Email must be between 8 and 60 characters.")] 
+        [BindProperty]
+        [StringLength(60, MinimumLength = 8, ErrorMessage = "Email must be between 8 and 60 characters.")]
         [Required] public string Email { get; set; }
 
-        [BindProperty] 
-        [StringLength(20, MinimumLength = 5, ErrorMessage = "Password must be between 5 and 20 characters.")] 
+        [BindProperty]
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "Password must be between 5 and 20 characters.")]
         [Required] public string Password { get; set; }
 
-        [BindProperty] 
+        [BindProperty]
         [Compare("Password", ErrorMessage = "Retype password doesn't match, Type again!")]
         [Required] public string RetypePassword { get; set; }
 
+        [BindProperty] public bool CreateAccountFailed { get; private set; }
+
         public IActionResult OnPostAsync()
         {
-            User u = new User(1, Email, Password, Name, false);
+            try
+            {
+                //insert the user into the database
+                //await CreateUser(Email, Password, Name);
+            } catch (Exception e)
+            {
+                CreateAccountFailed = true;
+                return Page();
+            }
             //send the user to the database
             return RedirectToPage("Login");
         }
