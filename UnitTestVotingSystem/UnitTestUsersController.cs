7e9ab9a8ce7f9248b61c7d134912606987b8c2c4
CameronCollingham <Tacopwrphd@gmail.com>
2021-04-24 02:37:25 -0500

    Create UnitTestUsersController.cs
    UsersController Unit tester. Cannot be finished until UsersController is changed into standard Controller from (such as Issues or Elections)

---
+++ UnitTestVotingSystem/UnitTestUsersController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VotingSystem.Controllers;
+using VotingSystem.Data;
+using VotingSystem.Models;
+
+namespace UnitTestVotingSystem
+{
+    [TestClass]
+    class UnitTestUsersController
+    {
+        private readonly VotingSystemContext context;
+
+        /*
+        * UsersController isn't able to be instantiated in a controller's usual way
+        * Test below will not function until turned into true Controller form
+        */
+
+        //[TestMethod]
+        //public void GetAllUsers_ReturnsUsers()
+        //{
+
+        //    var controller = new UsersController(context);;
+        //    var result = controller.GetAllUsers;
+        //    Assert.Something
+        //}
+    }
+}
